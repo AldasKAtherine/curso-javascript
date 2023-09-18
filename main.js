62e6281152630fb9674f9a6e3f2577ad44b87176
AldasKAtherine <kaldas2401@uta.edu.ec>
2023-09-18 01:31:36 -0500

    carrito

--- main.js
+++ main.js
@@ -2,14 +2,37 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', togglewDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
 function togglewDesktopMenu(){
     //funcion toggle remueve o aniade la clase que se establece
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
 desktopMenu.classList.toggle("inactive");
 }
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside(){
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+    
+    if (!isMobileMenuClose) {
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+    /*aniadir validacion de la cuenta*/
+
+   
+
 }
