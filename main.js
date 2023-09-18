dadcbd443fd62c44b67465ed13e2048cc029b8a2
AldasKAtherine <kaldas2401@uta.edu.ec>
2023-09-18 01:04:23 -0500

    mobile-menu

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
-
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', togglewDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function togglewDesktopMenu(){
     //funcion toggle remueve o aniade la clase que se establece
 desktopMenu.classList.toggle("inactive");
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
